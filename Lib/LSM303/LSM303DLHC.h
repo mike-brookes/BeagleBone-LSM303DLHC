c9dc1e88f059eb214e5e2110431aa5e67c7ab624
Michael Brookes <mbircohoakeels@me.com>
2016-06-12 18:53:31 +0100

    adding license info

--- Lib/LSM303/LSM303DLHC.h
+++ Lib/LSM303/LSM303DLHC.h
@@ -1,9 +1,19 @@
-//  LSM303DLHC.h
-//  BBBLSM303DLHC
-//
-//  Created by Michael Brookes on 25/10/2015.
-//  Copyright © 2015 Michael Brookes. All rights reserved.
-//
+/*
+Copyright (C) 2016 Michael Brookes
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef SRC_LSM303DLHC_H_
 #define SRC_LSM303DLHC_H_
--- example.cpp
+++ example.cpp
@@ -1,10 +1,21 @@
 /*
- * test.cpp
- *
- *  Created on: 25 Oct 2015
- *      Author: Michael Brookes
- *      Version 2.0 - 4/4/2016
- *
+/*
+Copyright (C) 2016 Michael Brookes
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*
  *      Example of using the LSM303DLHC class
  *      The accelerometer class creates an object and initialises the accelerometer on the LSM303
  *      It then uses the Macros available from the LSM303DLHC.h to configure the device
